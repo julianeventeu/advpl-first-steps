c0653c10a5a192a02866b0bf1a8a541b3ade5fc2
Juliane Regina Venteu <juliane.venteu@sp01.local>
2019-07-17 08:53:06 -0300

    removido itens desnecessarios para execucao do python dentro do container

--- build/tests/runTest.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "run..."
-echo $1
-/usr/local/FastCompile --compileID=F4DC-CC99 --passwordEmpty --build=170117A --server=localhost --enviroment=ENVIRONMENT --port=1234 --testcase=$1 --authorization=/aut/auto.aut --enableCoverage
